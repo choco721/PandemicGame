fad590f71790530143191d8a5dd0132c762790ec
Agustin Scellato <scellatoagustin@gmail.com>
2024-09-09 20:09:30 -0300

    ejemplo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,17 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        GestorCartas gestorCartas = new GestorCartas();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Robar cartas y mostrar resultados
+        String cartaInfeccion = gestorCartas.robarCartaInfeccion();
+        System.out.println("Carta de Infección Robada: " + cartaInfeccion);
+
+        String cartaEvento = gestorCartas.robarCartaEvento();
+        System.out.println("Carta de Evento Robada: " + cartaEvento);
+
+        // Mostrar estado de las pilas de descarte
+        System.out.println("Cartas de infección descartadas: " + gestorCartas.obtenerPilaDescarteInfeccion());
+        System.out.println("Cartas de evento descartadas: " + gestorCartas.obtenerPilaDescarteEventos());
     }
 }
